0db9c6c6cd8386993c7522e8199ba5cb922972ee
joaowoigt <joaowoigt@gmail.com>
2021-07-11 22:18:38 -0300

    limpando

--- listadeContatos/app/src/main/java/com/woigt/listadecontatos/features/model/ContaosVO.kt
+++
@@ -1,7 +0,0 @@
-package com.woigt.listadecontatos
-
-data class ContatosVO(
-    var id: Int = -1,
-    var nome: String = "",
-    var telefone: String = ""
-)
--- listadeContatos/app/src/main/java/com/woigt/listadecontatos/singleton/ContatoSingleton.kt
+++
@@ -1,8 +0,0 @@
-package com.woigt.listadecontatos.singleton
-
-import com.woigt.listadecontatos.ContatosVO
-
-// import contatos vo
-object ContatoSingleton {
-    var lista: MutableList<ContatosVO> = mutableListOf()
-}
